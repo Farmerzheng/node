dd602c3483292b944f963911d826fc7595ecd3ef
wangzheng <1072085294@qq.com>
2018-05-21 20:30:17 +0800

    2018-5-21

--- shopping_mall/Client/config/index.js
+++ shopping_mall/Client/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
         // Paths
         assetsSubDirectory: 'static',
-        assetsPublicPath: '/',
+        assetsPublicPath: './',
 
         // Various Dev Server settings
         host: 'localhost', // can be overwritten by process.env.HOST
@@ -53,7 +53,7 @@ module.exports = {
         // Paths
         assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: '/',
+        assetsPublicPath: './',
 
         /**
          * Source Maps
